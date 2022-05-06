f38511a1f7e550963403b52030d2d648ee296cbe
frakko729 <frank.koehn97@gmail.com>
2022-05-06 14:29:08 +0200

    save jobs on backend

--- src/views/jobs/JobCreate.vue
+++ src/views/jobs/JobCreate.vue
@@ -18,33 +18,37 @@ import { PlusIcon, CloudIcon } from "@heroicons/vue/solid";
 import KeyValueTable from "../../components/tables/KeyValueTable.vue";
 import JsonOutput from "../../components/JsonOutput.vue";
 import { useGeneralStore } from "@/stores/generalStore";
+import { useApi } from "@/composables/api";
 
+/**
+ * Get chain data from backend
+ */
 const chainStore = useChainStore();
 chainStore.loadChains();
 
+/**
+ * Import general store for resolving backend images
+ */
+const generalStore = useGeneralStore();
+
+/**
+ * Static select data
+ */
 const methods = [
   { id: 1, name: "GET" },
   { id: 2, name: "POST" },
 ];
+const jobTypes = [{ id: 1, name: "Direct Request" }];
 
-const addKeyValuePair = (appendTo: Array<any>) => {
-  let id = 0;
-
-  if (appendTo.length !== 0) {
-    id = appendTo[appendTo.length - 1].id + 1;
-  }
-  appendTo.push({
-    id: id,
-    key: "",
-    value: "",
-    isActive: true,
-  });
-};
-
+/**
+ * Reactive user input data
+ */
 const jobData = reactive({
+  jobType: jobTypes[0],
+  name: "",
   url: "",
   method: methods[0],
-  chain: undefined,
+  chain: {} as Backend.Models.Chain,
   parms: [
     {
       id: 0,
@@ -63,23 +67,139 @@ const jobData = reactive({
   ],
 });
 
+/**
+ * Computed counts for tab
+ * TODO don't count empty entries
+ */
 const parmsCount = computed((): number => jobData.parms.length);
 const headersCount = computed((): number => jobData.headers.length);
 
+/**
+ * Tabs definition
+ */
 const tabs: Array<any> = [
   { name: "Parameters", href: "#", count: parmsCount, current: true },
   { name: "Headers", href: "#", count: headersCount, current: false },
   { name: "Test", href: "#", current: false },
 ];
 
-const isValid = ref(false);
+/**
+ * Adds new entry for parms or headers
+ *
+ * @param appendTo
+ */
+const addKeyValuePair = (appendTo: Array<any>) => {
+  let id = 0;
 
-const generalStore = useGeneralStore();
+  if (appendTo.length !== 0) {
+    id = appendTo[appendTo.length - 1].id + 1;
+  }
+  appendTo.push({
+    id: id,
+    key: "",
+    value: "",
+    isActive: true,
+  });
+};
+
+const isValid = ref(true);
+
+/**
+ * Send data to backend
+ */
+const { post, data, loading, error } = useApi("api/jobs");
+
+const onSubmit = async () => {
+  await post({
+    name: jobData.name,
+    method: jobData.method.name,
+    url: jobData.url,
+    job_type_id: jobData.jobType.id,
+    chain_id: jobData.chain.id,
+    parameters: JSON.stringify(jobData.parms),
+    headers: JSON.stringify(jobData.headers),
+  });
+
+  console.log(error);
+  console.log(data);
+};
 </script>
 
 <template>
   <Container>
-    <div class="flex">
+    <div class="flex space-x-4">
+      <!-- Chainlink Job Type Input Start -->
+      <Listbox as="div" v-model="jobData.jobType" class="w-1/2">
+        <div class="relative">
+          <ListboxButton
+            class="h-12 bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:border-blue-500 sm:text-sm"
+          >
+            <span class="block truncate">{{ jobData.jobType.name }}</span>
+            <span
+              class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none"
+            >
+              <SelectorIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+            </span>
+          </ListboxButton>
+
+          <transition
+            leave-active-class="transition ease-in duration-100"
+            leave-from-class="opacity-100"
+            leave-to-class="opacity-0"
+          >
+            <ListboxOptions
+              class="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base focus:outline-none focus:border-blue-500 sm:text-sm"
+            >
+              <ListboxOption
+                as="template"
+                v-for="jobType in jobTypes"
+                :key="jobType.id"
+                :value="jobType"
+                v-slot="{ active, selected }"
+              >
+                <li
+                  :class="[
+                    active ? 'text-white bg-blue-600' : 'text-gray-900',
+                    'cursor-default select-none relative py-2 pl-3 pr-9',
+                  ]"
+                >
+                  <span
+                    :class="[
+                      selected ? 'font-semibold' : 'font-normal',
+                      'block truncate',
+                    ]"
+                  >
+                    {{ jobType.name }}
+                  </span>
+
+                  <span
+                    v-if="selected"
+                    :class="[
+                      active ? 'text-white' : 'text-blue-600',
+                      'absolute inset-y-0 right-0 flex items-center pr-4',
+                    ]"
+                  >
+                    <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                  </span>
+                </li>
+              </ListboxOption>
+            </ListboxOptions>
+          </transition>
+        </div>
+      </Listbox>
+      <!-- Chainlink Job Type Input End -->
+
+      <!-- Job Name Start -->
+      <input
+        class="h-12 w-1/2 px-4 rounded-md border border-gray-300 shadow-sm focus:outline-none focus:border-blue-500"
+        placeholder="Job name"
+        v-model="jobData.name"
+      />
+      <!-- Job Name End -->
+    </div>
+
+    <div class="flex mt-4">
+      <!-- Request Method Input Start -->
       <Listbox as="div" v-model="jobData.method">
         <div class="relative w-28">
           <ListboxButton
@@ -110,7 +230,7 @@ const generalStore = useGeneralStore();
               >
                 <li
                   :class="[
-                    active ? 'text-white bg-indigo-600' : 'text-gray-900',
+                    active ? 'text-white bg-blue-600' : 'text-gray-900',
                     'cursor-default select-none relative py-2 pl-3 pr-9',
                   ]"
                 >
@@ -126,7 +246,7 @@ const generalStore = useGeneralStore();
                   <span
                     v-if="selected"
                     :class="[
-                      active ? 'text-white' : 'text-indigo-600',
+                      active ? 'text-white' : 'text-blue-600',
                       'absolute inset-y-0 right-0 flex items-center pr-4',
                     ]"
                   >
@@ -138,17 +258,21 @@ const generalStore = useGeneralStore();
           </transition>
         </div>
       </Listbox>
+      <!-- Request Method Input End -->
 
+      <!-- Request Url Start -->
       <input
         class="h-12 w-full px-4 rounded-r-md border border-gray-300 shadow-sm focus:outline-none focus:border-blue-500"
         placeholder="https://external-api.dev/api/example"
         v-model="jobData.url"
       />
+      <!-- Request Url End -->
     </div>
 
-    <!-- Tabs -->
+    <!-- Tabs Start -->
     <Tabs :tabs="tabs">
       <template #header="{ activeTab }">
+        <!-- Parameters Tab Header Start -->
         <button
           v-if="activeTab?.name === 'Parameters'"
           @click="addKeyValuePair(jobData.parms)"
@@ -158,6 +282,9 @@ const generalStore = useGeneralStore();
           <PlusIcon class="sm:-ml-1 sm:mr-2 h-5 w-5" aria-hidden="true" />
           <span class="hidden sm:block">Add</span>
         </button>
+        <!-- Parameters Tab Header End -->
+
+        <!-- Headers Tab Header Start -->
         <button
           v-if="activeTab?.name === 'Headers'"
           @click="addKeyValuePair(jobData.headers)"
@@ -167,6 +294,9 @@ const generalStore = useGeneralStore();
           <PlusIcon class="sm:-ml-1 sm:mr-2 h-5 w-5" aria-hidden="true" />
           <span class="hidden sm:block">Add</span>
         </button>
+        <!-- Headers Tab Header End -->
+
+        <!-- Test Tab Header Start -->
         <button
           v-if="activeTab?.name === 'Test'"
           @click="addKeyValuePair(jobData.headers)"
@@ -176,9 +306,11 @@ const generalStore = useGeneralStore();
           <CloudIcon class="sm:-ml-1 sm:mr-2 h-5 w-5" aria-hidden="true" />
           <span class="hidden sm:block">Test</span>
         </button>
+        <!-- Test Tab Header End -->
       </template>
 
       <template #default="{ activeTab }">
+        <!-- Parameters Tab Start -->
         <div v-if="activeTab?.name === 'Parameters'">
           <KeyValueTable v-model="jobData.parms" />
 
@@ -187,15 +319,23 @@ const generalStore = useGeneralStore();
             more informations.</span
           >
         </div>
+        <!-- Parameters Tab End -->
+
+        <!-- Headers Tab Start -->
         <div v-if="activeTab?.name === 'Headers'">
           <KeyValueTable v-model="jobData.headers" />
         </div>
+        <!-- Headers Tab End -->
 
+        <!-- Test Tab Start -->
         <div v-if="activeTab?.name === 'Test'"><JsonOutput /></div>
+        <!-- Test Tab End -->
       </template>
     </Tabs>
+    <!-- Tabs End -->
 
     <div class="mt-8">
+      <!-- Chain Input Start -->
       <RadioGroup v-model="jobData.chain">
         <RadioGroupLabel class="text-base font-medium text-gray-900">
           Select a chain
@@ -206,7 +346,7 @@ const generalStore = useGeneralStore();
             as="template"
             v-for="chain in chainStore.chains"
             :key="chain.id"
-            :value="chain.id"
+            :value="chain"
             v-slot="{ checked, active }"
           >
             <div
@@ -264,10 +404,12 @@ const generalStore = useGeneralStore();
           </RadioGroupOption>
         </div>
       </RadioGroup>
+      <!-- Chain Input End -->
     </div>
 
     <div class="w-full mt-8">
       <button
+        @click="onSubmit()"
         :disabled="!isValid"
         type="button"
         :class="{
